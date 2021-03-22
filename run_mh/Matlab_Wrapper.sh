6689934c36b23513a664e821fe631e317d435d89
William Raymond <wsraymon@rams.colostate.edu>
2021-03-22 10:31:51 -0600

    moving old fits to unneeded

--- run_mh/Matlab_Wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-#/usr/local/MATLAB/R2015b/bin/matlab -nosplash -nodisplay -r "i=6;j=3;Batch_Matlab_Example(i,j,'Results/Output');exit;" > Logs/Log_6_3.txt
-/usr/local/MATLAB/R2020a/bin/matlab -nosplash -nodisplay -r "i=${i},run_mh_cluster(i);exit;" > Logs/Log_${i}.txt
